d18fb31ec7ec41d99f1981b1071712c2c8f20f09
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-09 15:11:36 +0400

    feat: add Department and Employee models

---
+++ src/apps/staff/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from apps.base.models import BaseModel
+
+
+class Department(BaseModel):
+    name = models.CharField(max_length=128, help_text="Department")
+
+
+class Employee(BaseModel):
+    user = models.ForeignKey(to="users.User", on_delete=models.CASCADE, help_text="User")
+    department = models.ForeignKey(
+        to=Department, on_delete=models.CASCADE, help_text="Department"
+    )
