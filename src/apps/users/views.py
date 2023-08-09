dbc26df8cfec8ce6b4fd1ddeb0239e1a1f7b31d1
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-09 11:51:26 +0400

    feat: add view for refresh token

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.models import User
-from apps.users.serializers import LoginSerializer
+from apps.users.serializers import LoginSerializer, RefreshTokenSerializer
 from utils.auth import TokenVerification
 
 
@@ -22,3 +22,17 @@ class LoginAPIView(APIView):
         response_data = dict(access_token=access_token, refresh_token=refresh_token)
 
         return Response(response_data)
+
+
+class RefreshTokenAPIView(APIView):
+    serializer_class = RefreshTokenSerializer
+
+    def post(self, request, *args, **kwargs):  # noqa
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        payload = serializer.data.get("payload")
+
+        access_token = TokenVerification.create_access_token(user_id=payload.get("sub"))
+
+        return Response(data={"access_token": access_token})
