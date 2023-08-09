3071dfa9d8651122aaac32016bca8ae429b4ffd3
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-09 11:31:25 +0400

    feat: add custom exceptions module for users app

---
+++ src/utils/exceptions.py
@@ -0,0 +1,32 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class PasswordMismatchException(APIException):
+    """
+    raise if password and password_confirm mismatch.
+    """
+
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Password and Confirm Password fields must have the same value."
+    default_code = "bad_request"
+
+
+class InvalidEmailOrPasswordException(APIException):
+    """
+    raise if email or password is invalid.
+    """
+
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Invalid email or password."
+    default_code = "bad_request"
+
+
+class InvalidTokenTypeException(APIException):
+    """
+    raise if token type is wrong invalid.
+    """
+
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = "Invalid token type."
+    default_code = "bad_request"
