d2a78adff101ad58a65620829a35aa9e93f78cf2
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-09 11:33:51 +0400

    feat: add serializer for user login

--- src/apps/users/serializers.py
+++ src/apps/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.users.models import User
+from utils.exceptions import InvalidEmailOrPasswordException
 from utils.validators import PasswordMatchValidator
 
 
@@ -27,3 +28,23 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop("password_confirm")
         instance = User.objects.create_user(**validated_data)
         return instance
+
+
+class LoginSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField(read_only=True)
+    email = serializers.EmailField(max_length=128, help_text="Email", write_only=True)
+
+    password = serializers.CharField(
+        max_length=128, help_text="Password", write_only=True
+    )
+
+    def validate(self, attrs: dict):
+        attrs = super().validate(attrs)
+
+        user = User.objects.filter(email=attrs.get("email")).first()
+        if not (user and user.check_password(attrs.get("password"))):
+            raise InvalidEmailOrPasswordException()
+
+        attrs["user_id"] = user.id
+
+        return attrs
