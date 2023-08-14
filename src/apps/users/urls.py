7e9c3c8e6a53cc41b34cd72dc73f339c719ad410
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-14 17:27:07 +0400

    refactor: remove RegisterAPIView from users urls

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from apps.users.views import LoginAPIView, RefreshTokenAPIView, RegisterAPIView
+from apps.users.views import LoginAPIView, RefreshTokenAPIView
 
 urlpatterns = [
-    path("register/", RegisterAPIView.as_view(), name="user-register"),
     path("login/", LoginAPIView.as_view(), name="user-login"),
     path("refresh/", RefreshTokenAPIView.as_view(), name="refresh-token"),
 ]
