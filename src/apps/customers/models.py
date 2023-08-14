e6c4f5aa6b5a3cf74c1cdd09777669b32e4a5d66
roma_gudavadze <roma.gudavadze@gmail.com>
2023-08-14 17:26:35 +0400

    feat: add Customer model and create migration file according to that.

---
+++ src/apps/customers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from apps.base.models import BaseModel
+
+
+class Customer(BaseModel):
+    user = models.OneToOneField(
+        to="users.User",
+        on_delete=models.CASCADE,
+        help_text="User",
+    )
+    identity_number = models.CharField(max_length=128, help_text="Identity Number")
+    name = models.CharField(max_length=128, help_text="Name")
+
+    def __str__(self):
+        return f"{self.name} - {self.identity_number}"
